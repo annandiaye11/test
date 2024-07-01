94bce2c166eda9386685bf0b97d427cce2be84cc
TanakAiko <cherifmbaye02@gmail.com>
2024-07-01 15:57:13 +0000

    createPost modal done, continuz the work

--- static/index.js
+++ static/index.js
@@ -1,5 +1,7 @@
 const createPostButton = document.getElementById('createPostButton');
 const modal = document.getElementById('createPostModal')
+const createPostForm = document.getElementById('createPostForm')
+const categorieError = document.getElementById('categorieError');
 
 function openModal() {
     modal.style.display = 'block';
@@ -15,4 +17,39 @@ window.onclick = function(event) {
     }
 }
 
-createPostButton.addEventListener('click', openModal)
+createPostButton.addEventListener('click',function (event) {
+    event.preventDefault();
+    openModal();
+})
+
+createPostForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const checkboxes = createPostForm.querySelectorAll('input[name="options"]:checked');
+    if (checkboxes.length === 0) {
+        categorieError.style.display = 'block';
+        return; // Empêcher la soumission du formulaire
+    } else {
+        categorieError.style.display = 'none';
+    }
+
+    const data = getDataForm(createPostForm)
+
+    console.log(data);
+})
+
+
+function getDataForm(form) {
+    const dataForm = new FormData(form);
+    const data = Object.fromEntries(dataForm.entries());
+
+    const checkboxes = form.querySelectorAll('input[type="checkbox"]:checked');
+    data.options = Array.from(checkboxes).map(checkbox => checkbox.value);
+
+    const fileInput = form.querySelector('input[type="file"]');
+    if (fileInput.files.length > 0) {
+        data.file = fileInput.files[0];
+    }
+
+    return data;
+}
